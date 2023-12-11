011b7f64a433e3cab82176fd1dc7d08b5970ff67
g-jaykrishnan <jaykrishnang@campus.technion.ac.il>
2023-12-11 18:18:47 +0530

    added setup.py, edited README.md, todo remove README.rst

--- README.md
+++ README.md
@@ -8,13 +8,47 @@ Though EPyT can be employed for performing water quality analysis, which comes w
 	
  The default modules of EPyT-C conceptualize scientific knowledge about the physical, physicochemical, and biochemical exchanges concerning water quality, mathematize them as partial differential equations (PDE), and numerically solve them to simulate the spatiotemporal distribution of complex water quality parameters in WDS. EPyT-C employs the numerical method initially presented by (Tzatchkov et al., 2002), which solves the advective-reactive equations by combining the explicit method of characteristics and the fourth-order Runge-Kutta method.
 
-# Chlorine decay and Trihalomethanes formation module
+## Chlorine decay and Trihalomethanes formation module
 The ‘Chlorine decay and Trihalomethanes formation’ module of the EPyT-C encompasses a one-dimensional mechanistic MSRT model integrating the advective transport and physicochemical exchanges of the following three water quality parameters: free available chlorine (FAC), total organic carbon (TOC), and trihalomethanes (THMs).
 
-# Bacterial regrowth module
+## Bacterial regrowth module
 The ‘Bacterial regrowth’ module of the EPyT-C encompasses a one-dimensional mechanistic MSRT model integrating the advective transport and physicochemical exchanges of the following five water quality parameters: free available chlorine (FAC), recalictrant dissolved organic carbon (RDOC), biodegradable dissolved organic carbon (BDOC), free living bacteria (FLB), and free dead bacteria (FDB).
+
+# Flexibilities
+
+EPyT-C offers the following flexibilities, making it a handy tool for research and industry:
+- Allows time-series variations in the input values for the water quality parameters at the sources (reservoirs and booster nodes).
+- Customize the random fluctuations in the input values for the water quality parameters at the sources.
+- Customize the perturbations in the reaction rate coefficient values.
+- Customize the outputs and export the data as Excel files or other formats.
+- Customize the numerical accuracy by altering the model parameters (time step, velocity tolerance, etc.).
+- Control the computational efficiency by adjusting the accuracy of the numerical solutions.
+
+# Installation
+
+To install EPyT-C, 
+
+`pip install epyt_c`
+    
+Alternatively, the sources for EPyT-C can be downloaded from the Github repo. You can clone the public repository: https://github.com/grabhijith/EPyT-C
+
+# Dependencies
+- EPyT 
+- NumPy 1.2.6
+- Pandas 2.1.3
+- XlsxWriter 3.1.9
 
 # References
 Rossman, L.A., Woo, H., Tryby, M., Shang, F., Janke, R., Haxton, T., 2020. Epanet 2.2 User ’s Manual, USEPA. Cincinnati, Ohio.
 
 Tzatchkov, V.G., Aldama, A.A., Arreguin, F.I., 2002. Advection-Dispersion-Reaction Modeling in Water Distribution Networks. J. Water Resour. Plan. Manag. 128, 334–342. https://doi.org/10.1061/(asce)0733-9496(2002)128:5(334)
+
+# Contact
+
+**Gopinbathan R Abhijith** - abhijith@iitk.ac.in
+
+**Avi Ostfeld** - ostfeld@technion.ac.il
+
+# Credits 
+
+The **Smart Water Infrastructure Laboratory**, **Indian Institute of Technology Kanpur** and **Technion Israel Institute of Technology** jointly created this package.
---
+++ setup.py
@@ -0,0 +1,101 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="epyt_c",
+    version="0.0.1",
+    description="An open-source Python package for modeling water quality in water distribution systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grabhijith/EPyT-C",
+    author="Gopinbathan R Abhijith",
+    author_email="abhijith@iitk.ac.in",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",  # todo - specify the supported python versions
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="EPANET, EPyT, water quality modelling",
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={"": "src"},  # Optional todo
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where="src"),  # Required
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. See
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires=">=3.7, <4",
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/discussions/install-requires-vs-requirements/
+    install_requires=["peppercorn"],  # Optional
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    },
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    package_data={  # Optional
+        "sample": ["package_data.dat"],
+    },
+    # Entry points. The following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        "console_scripts": [
+            "sample=sample:main",
+        ],
+    },
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/pypa/sampleproject/issues",
+        "Funding": "https://donate.pypi.org",
+        "Say Thanks!": "http://saythanks.io/to/example",
+        "Source": "https://github.com/pypa/sampleproject/",
+    },
+)
